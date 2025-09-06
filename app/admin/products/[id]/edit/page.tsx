ec25af75c30e975df42a9ccb3c7c314594aab946
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-09-06 15:03:29 -0700

    [ADD] Funcion para editar productos

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import { prisma } from "@/src/lib/prisma";
+import { notFound } from "next/navigation";
+import Heading from "../../../../../components/UI/Heading";
+import EditProductForm from "../../../../../components/products/EditProductForm";
+import ProductForm from "@/components/products/ProductForm";
+import GoBackButton from "@/components/UI/GoBackButton";
+
+async function getProductById(id: number) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!product) {
+    notFound();
+  }
+  return product;
+}
+export default async function editProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const product = await getProductById(+params.id);
+  return (
+    <>
+      <Heading>Editar producto: {product.name}</Heading>
+
+      <GoBackButton />
+
+      <EditProductForm>
+        <ProductForm product={product} />
+      </EditProductForm>
+    </>
+  );
+}
--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma"
 import ImageUpload from "./ImageUpload"
+import { Product } from "@prisma/client"
 
 async function getCategories() {
     return await prisma.category.findMany({
@@ -7,7 +8,11 @@ async function getCategories() {
     })
 }
 
-export default async function ProductForm() {
+type ProductFormProps = {
+    product?: Product
+}
+
+export default async function ProductForm({product} : ProductFormProps) {
 
     const categories = await getCategories()
 
@@ -24,6 +29,7 @@ export default async function ProductForm() {
                     name="name"
                     className="block w-full p-3 bg-slate-100"
                     placeholder="Nombre Producto"
+                    defaultValue={product?.name}
                 />
             </div>
 
@@ -37,6 +43,7 @@ export default async function ProductForm() {
                     name="price"
                     className="block w-full p-3 bg-slate-100"
                     placeholder="Precio Producto"
+                    defaultValue={product?.price}
                 />
             </div>
 
@@ -49,6 +56,7 @@ export default async function ProductForm() {
                     className="block w-full p-3 bg-slate-100"
                     id="categoryId"
                     name="categoryId"
+                    defaultValue={product?.categoryId}
                 >
                     <option value="">-- Seleccione --</option>
                     {categories.map(category => (
@@ -57,10 +65,11 @@ export default async function ProductForm() {
                             value={category.id}
                         >{category.name}</option>
                     ))}
-          
                 </select>
             </div>
-            <ImageUpload />
+            <ImageUpload 
+                image={product?.image}
+            />
         </>
     )
 }
